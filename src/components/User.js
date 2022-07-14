88a70d7c2d2a564859e699e26acb9e1694ed514a
Daphne <daphneruth304@gmail.com>
2022-07-14 16:51:08 +0300

    created a class

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,11 @@
 import classes from './User.module.css';
+import { Component } from 'react/cjs/react.production.min';
 
+class user extends Component {
+  render() {
+    return <li Component={classes.user}>{this.props.name}</li>
+  }
+}
 const User = (props) => {
   return <li className={classes.user}>{props.name}</li>;
 };
